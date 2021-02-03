55ce9a48044fbb2e40c5b3e205fd8f690d7863a8
Idan <idan.ergaz@gmail.com>
2021-02-03 14:25:52 +0200

    my 2nd commit

--- test_practice1.py
+++ test_practice1.py
@@ -45,7 +45,6 @@ def test_hover():
     action=ActionChains(driver)
     action.move_to_element(time).move_to_element(attendence).click(myRecords).perform()
 
-adding my comment007
 @pytest.mark.regression
 def test_tearDown():
     print('Finish runing the test!')
