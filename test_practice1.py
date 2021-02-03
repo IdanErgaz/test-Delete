492bab1927052473cf1872c54a8cc49f1b3ca668
Idan <idan.ergaz@gmail.com>
2021-02-03 18:18:04 +0200

    comment6

--- test_practice1.py
+++ test_practice1.py
@@ -1,4 +1,5 @@
-import pytest,time
+import pytest
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -8,6 +9,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 ######################################################################
 @pytest.mark.regression
+@pytest.mark.sanity
 def test_setUp():
     print('Starting a new test...')
     global driver
@@ -30,7 +32,7 @@ def test_login():
     wait.until(EC.text_to_be_present_in_element((By.ID, 'logInPanelHeading'), 'LOGIN Panel'))
     time.sleep(3)
 
-
+@pytest.mark.sanity
 def test_hover():
     driver.find_element_by_css_selector('input[name="txtUsername"]').clear()
     driver.find_element_by_css_selector('input[name="txtUsername"]').send_keys('Admin')
@@ -39,14 +41,16 @@ def test_hover():
     driver.find_element_by_id('btnLogin').click()
     wait = WebDriverWait(driver, 10)
     wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'div.head'), "Dashboard"))
-    time=driver.find_element_by_id('menu_time_viewTimeModule')
+    timeElement=driver.find_element_by_id('menu_time_viewTimeModule')
     attendence=driver.find_element_by_id('menu_attendance_Attendance')
     myRecords=driver.find_element_by_id('menu_attendance_viewMyAttendanceRecord')
     action=ActionChains(driver)
-    action.move_to_element(time).move_to_element(attendence).click(myRecords).perform()
-
+    action.move_to_element(timeElement).move_to_element(attendence).click(myRecords).perform()
+    time.sleep(5)
 #just adding a comment- commit and check auto test running
 @pytest.mark.regression
+@pytest.mark.sanity
+
 def test_tearDown():
     print('Finish runing the test!')
     driver.quit()
